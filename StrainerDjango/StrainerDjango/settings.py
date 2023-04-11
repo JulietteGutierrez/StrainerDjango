1f9192f77c91c6afd7ec91bea47d2f387ae75f95
JulietteGutierrez <111533290+JulietteGutierrez@users.noreply.github.com>
2023-04-11 10:59:45 -0300

    Update settings.py

--- StrainerDjango/StrainerDjango/settings.py
+++ StrainerDjango/StrainerDjango/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pg+6g!h5z#06!s^6bq3p65ql1@a3732w3-$*+acij-_lha^0o8'
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
