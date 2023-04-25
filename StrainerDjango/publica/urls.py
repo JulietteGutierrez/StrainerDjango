a4cf486ce54a9c287e7fdd2023d9d1459ffa95db
JulietteGutierrez <juli.gutierrez@live.com.ar>
2023-04-25 16:07:32 -0300

    elimino url duplicado de contacto

--- StrainerDjango/publica/urls.py
+++ StrainerDjango/publica/urls.py
@@ -5,13 +5,8 @@ urlpatterns = [
     path('', views.index, name='inicio'),
     path('origenes/',views.origenes,name='origenes'), 
     path('sucursales/',views.sucursales,name='sucursales'), 
-<<<<<<< HEAD
-    path('tienda-online/',views.tienda,name='tienda-online'),
-    path('contacto/',views.contacto,name='contacto'),
-=======
     path('contacto/',views.contacto,name='contacto'),
     path('tienda/',views.tienda,name='tienda'),
     path('tienda/producto/<int:id>', views.tiendaProducto, name='producto'),
     # path('tienda/carrito/<int:id>/<str:cantidad>', views.tiendaCarritoAgregar, name='carritoAgregar'),
->>>>>>> de9a26272c4f020ac8392e943a996a460db1903f
 ]
