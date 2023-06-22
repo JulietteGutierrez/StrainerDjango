65a14e4408a3e313dad5c9a98a4d9f4d8532dcb0
JulietteGutierrez <juli.gutierrez@live.com.ar>
2023-06-22 08:38:37 -0300

    se agrega descripcion de los productos en views.py

--- StrainerDjango/publica/views.py
+++ StrainerDjango/publica/views.py
@@ -201,21 +201,21 @@ def formatDictProducto(id, nombre, precio, id_categoria, imagen, descripcion):
            }
 
 def getProducto(id):
-    descripcionDefault = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    #descripcionDefault = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 
-    productos = (('Soporte Individual', 300, 5, 'soporte-bg.png', descripcionDefault),
-                 ('Pack Strainer x3 ', 850, 4, 'pack-bg.png', descripcionDefault),
-                 ('Pack Strainer x6', 1650, 4, 'pack2.png', descripcionDefault),
-                 ('Taza Strainer', 1700, 3, 'taza2.png', descripcionDefault),
-                 ('Cafetera Dolce Gusto', 24000,  1, 'cafetera-dolce.webp', descripcionDefault),
-                 ('Cafetera Oyster', 39000,  1, 'cafetera-oster.webp', descripcionDefault),
-                 ('Cápsulas Dolce Gusto Chococino', 2700, 2, 'capsulas-chococino.webp', descripcionDefault),
-                 ('Cápsulas Dolce Gusto Lungo', 2700, 2, 'capsulas-lungo.webp', descripcionDefault),
-                 ('Cápsulas Dolce Gusto Café con Leche', 2700, 2, 'capsulas-cafeconleche.webp', descripcionDefault),
-                 ('Remera Vaso', 2700, 3, 'remera-vaso.webp', descripcionDefault),
-                 ('Remera Astronauta Luna', 2700, 3, 'remera-astronauta-luna.webp', descripcionDefault),
-                 ('Remera Astronauta Taza', 2700, 3, 'remera-astronauta-cafe.webp', descripcionDefault),
-                 ('Remera Casa Cafetera', 2700, 3, 'remera-casa.webp', descripcionDefault))
+    productos = (('Soporte Individual', 300, 5, 'soporte-bg.png', "Un Soporte Individual con Blend de café a elección."),
+                 ('Pack Strainer x3 ', 850, 4, 'pack-bg.png', "Pack de tres soportes de café con Blend a elección."),
+                 ('Pack Strainer x6', 1650, 4, 'pack2.png', "Pack de seis soportes de café con Blend a elección."),
+                 ('Taza Strainer', 1700, 3, 'taza2.png', "Taza con el diseño de Gatito Strainer Coffee."),
+                 ('Cafetera Dolce Gusto', 24000,  1, 'cafetera-dolce.webp', "Cafetera para cápsulas de café Dolce Gusto"),
+                 ('Cafetera Oyster', 39000,  1, 'cafetera-oster.webp', "Cafetera para café espresso Oyster."),
+                 ('Cápsulas Dolce Gusto Chococino', 2700, 2, 'capsulas-chococino.webp', "Cápsulas de café Dolce Gusto, sabor Chocochino."),
+                 ('Cápsulas Dolce Gusto Lungo', 2700, 2, 'capsulas-lungo.webp', "Cápsulas de café Dolce Gusto, sabor Lungo."),
+                 ('Cápsulas Dolce Gusto Café con Leche', 2700, 2, 'capsulas-cafeconleche.webp', "Cápsulas de café Dolce Gusto, sabor Café con Leche"),
+                 ('Remera Vaso', 2700, 3, 'remera-vaso.webp', "Remera de Strainer con el diseño Vaso."),
+                 ('Remera Astronauta Luna', 2700, 3, 'remera-astronauta-luna.webp', "Remera de Strainer con el diseño Astronauta."),
+                 ('Remera Astronauta Taza', 2700, 3, 'remera-astronauta-cafe.webp', "Remera de Strainer con el diseño de Taza y Astronauta."),
+                 ('Remera Casa Cafetera', 2700, 3, 'remera-casa.webp', "Remera de Strainer con el diseño de Cafetera."))
 
     if (id > 0) and (id <= len(productos)):
         return tuple((id, productos[id-1][0], productos[id-1][1], productos[id-1][2], productos[id-1][3], productos[id-1][4]))
